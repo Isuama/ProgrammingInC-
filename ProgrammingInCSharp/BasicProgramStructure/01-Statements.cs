ecc3631fd94c2377ede2f5eac886b43ba0ab6d07
Isuru Amarathunga <isuama.amarathunga@gmail.com>
2017-07-01 17:41:00 +0530

    End of the first and second sub modules

--- ProgrammingInCSharp/BasicProgramStructure/01-Statements.cs
+++ ProgrammingInCSharp/BasicProgramStructure/01-Statements.cs
@@ -8,7 +8,39 @@ namespace BasicProgramStructure
     {
         public _01_Statements()
         {
-            Console.WriteLine("1st lesson");
+            //variable declaration statements
+            int counter;
+            float distance;
+            string firstName;
+            
+            //assignment statements
+            counter = 0;
+            distance = 4.5F;
+            firstName = "Isuru";
+            
+            //jump statements
+            /*
+            break;
+            return;
+            */
+        }
+
+        void EmptyStatement()
+        {
+            // the empty statement consisit of a single semicolan on a line by itself 
+            // the statement does nothing and is merely a placeholder where a code
+            //statement is required but you don't want an action to take place. 
+            //A good example of this in a looping statement using in a delyaed processing scenario
+            while (DoSomething())
+                ;
+
+            // C# recognizes a statement that contains no keyword but because the semicolon is present,
+            // C# recognizes it as a statement
+        }
+
+        bool DoSomething()
+        {
+            return false;
         }
     }
 }
---
+++ ProgrammingInCSharp/BasicProgramStructure/02-ComplexStatements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgramStructure
+{
+    class _02_ComplexStatements
+    {
+        //complex statement are those that can or will enclose one or more simple statements into a code block 
+        //surrounded by curly braces {}
+        //an exmaple
+
+        void ComplexState()
+        {
+            //check to see how many values in an array are even numbers
+            int[] numbers = { 5, 24, 36, 19, 45, 60, 78 };
+            int evenNums = 0;
+
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine("Number {0} is an even number", num);
+                    evenNums++;
+                }
+                else
+                    Console.WriteLine("Number {0} is an odd number", num);
+
+                Console.WriteLine("there were {0} even number/s", evenNums);
+
+                //complex statements do not end with a semicolon
+            }
+        }
+    }
+    
+}
