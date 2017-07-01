2a5b61b0578397fa223b4e90a2d2921b8a5e33ea
Isuru Amarathunga <isuama.amarathunga@gmail.com>
2017-07-01 17:02:27 +0530

    Lesson 01 - Structure

---
+++ ProgrammingInCSharp/BasicProgramStructure/01-Statements.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgramStructure
+{
+    class _01_Statements
+    {
+        public _01_Statements()
+        {
+            Console.WriteLine("1st lesson");
+        }
+    }
+}
---
+++ ProgrammingInCSharp/BasicProgramStructure/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicProgramStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _01_Statements ls1 = new _01_Statements();
+            Console.ReadLine();
+        }
+    }
+}
